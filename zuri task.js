4ecd88dba8a210c47a878e0cef461b8a862cfd47
adeyemiayomide <aystix2@gmail.com>
2022-06-24 12:45:23 -0700

    my work

---
+++ zuri task.js
@@ -0,0 +1,12 @@
+const name = "Ayomide Adeyemi" ;
+console.log (name);
+document.write (name+'</br>');
+
+
+const height = "6ft"
+console.log (height);
+document.write (height+'</br>');
+
+const country = "Nigeria"
+console.log (country);
+document.write (country);
